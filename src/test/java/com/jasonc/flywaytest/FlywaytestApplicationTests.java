2cf84b8ffb696282e44b5a66ab7e86850a0bdc78
Jason_Chan <jasonc9702@gmail.com>
2022-03-06 22:34:54 +0800

    Initial commit

---
+++ src/test/java/com/jasonc/flywaytest/FlywaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonc.flywaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
